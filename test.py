12e646a7dbb9458ef4fa3ff348ac453688b50dec
FlyingBlackShark <aliu2000@outlook.com>
2024-06-05 06:02:28 +0000

    test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from jax.lax import with_sharding_constraint
 from jax.experimental import mesh_utils
 
 # Create a mesh and annotate each axis with a name.
-device_mesh = mesh_utils.create_device_mesh((2, 4))
+device_mesh = mesh_utils.create_device_mesh((4, 4))
 print(device_mesh)
 
 mesh = Mesh(devices=device_mesh, axis_names=('data', 'model'))
