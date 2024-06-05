ca3ea4db2fbaa84519e8279961dab83b2138ee92
FlyingBlackShark <aliu2000@outlook.com>
2024-06-05 06:01:47 +0000

    test

--- test.py
+++ test.py
@@ -1,18 +1,13 @@
-# The following code snippet will be run on all TPU hosts
-import jax
+from jax.sharding import Mesh, PartitionSpec, NamedSharding
+from jax.lax import with_sharding_constraint
+from jax.experimental import mesh_utils
 
-# The total number of TPU cores in the Pod
-device_count = jax.device_count()
+# Create a mesh and annotate each axis with a name.
+device_mesh = mesh_utils.create_device_mesh((2, 4))
+print(device_mesh)
 
-# The number of TPU cores attached to this host
-local_device_count = jax.local_device_count()
+mesh = Mesh(devices=device_mesh, axis_names=('data', 'model'))
+print(mesh)
 
-# The psum is performed over all mapped devices across the Pod
-xs = jax.numpy.ones(jax.local_device_count())
-r = jax.pmap(lambda x: jax.lax.psum(x, 'i'), axis_name='i')(xs)
-
-# Print from a single host to avoid duplicated output
-if jax.process_index() == 0:
-    print('global device count:', jax.device_count())
-    print('local device count:', jax.local_device_count())
-    print('pmap result:', r)
+def mesh_sharding(pspec: PartitionSpec) -> NamedSharding:
+  return NamedSharding(mesh, pspec)
