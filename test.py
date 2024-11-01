2aae2ea09782d8ced1c020876024519ebcae45d1
FlyingBlackShark <aliu2000@outlook.com>
2024-11-01 06:54:50 +0000

    test

---
+++ test.py
@@ -0,0 +1,19 @@
+import jax
+import jax.numpy as jnp
+from jax.sharding import Mesh, PartitionSpec, NamedSharding
+from jax.experimental import mesh_utils
+# 定义目标数组的形状
+shape = (4, 16)
+
+# 定义 sharding 策略（例如在 2 个设备上分配）
+device_mesh = mesh_utils.create_device_mesh((16,))
+mesh = Mesh(devices=device_mesh, axis_names=('data'))
+x_sharding = NamedSharding(mesh,PartitionSpec('data'))
+
+# 假设两个设备上各自持有 4x2 的数据
+arrays = jnp.ones((4))
+
+# 使用 make_array_from_single_device_arrays 来构造跨设备数组
+distributed_array = jax.make_array_from_single_device_arrays(shape, x_sharding, arrays)
+
+print(distributed_array)
