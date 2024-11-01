fe67393231540a60a9d3a2a41f3098d8fae80250
FlyingBlackShark <aliu2000@outlook.com>
2024-11-01 07:39:13 +0000

    test

--- test.py
+++ test.py
@@ -11,10 +11,7 @@ device_mesh = mesh_utils.create_device_mesh((8,))
 mesh = Mesh(device_mesh, axis_names=('data'))
 x_sharding = NamedSharding(mesh,PartitionSpec('data'))
 
-arrays = [
-    jnp.ones((4, 4)),
-    jnp.zeros((4, 4))
-]
+arrays = jnp.ones((4, 4))
 
 # 使用 make_array_from_single_device_arrays 来构造跨设备数组
 distributed_array = jax.make_array_from_single_device_arrays(shape, x_sharding, arrays)
