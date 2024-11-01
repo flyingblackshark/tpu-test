cd11a783d923dc267494701a74a7a6a625aa019a
FlyingBlackShark <aliu2000@outlook.com>
2024-11-01 07:29:02 +0000

    test

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ from jax.experimental import mesh_utils
 shape = (16,4)
 
 # 定义 sharding 策略（例如在 2 个设备上分配）
-device_mesh = mesh_utils.create_device_mesh((8,))
-#mesh = Mesh(devices=jax.devices(), axis_names=('data'))
-x_sharding = NamedSharding(device_mesh,PartitionSpec('data'))
+#device_mesh = mesh_utils.create_device_mesh((8,))
+mesh = Mesh(devices=jax.devices(), axis_names=('data'))
+x_sharding = NamedSharding(mesh,PartitionSpec('data'))
 
 # 假设两个设备上各自持有 4x2 的数据
-arrays = [jnp.ones((4,4)),jnp.ones((4,4)),jnp.ones((4,4)),jnp.ones((4,4))]
+arrays = [jnp.ones((4,4)),jnp.ones((4,4)))]
 
 # 使用 make_array_from_single_device_arrays 来构造跨设备数组
 distributed_array = jax.make_array_from_single_device_arrays(shape, x_sharding, arrays)
